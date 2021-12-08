fa5ff7221adbac11c26cd29f83c3ebd091f56a5a
KeitaroYabu <k.yabu@4nonome.com>
2021-12-08 23:48:14 +0900

    setting

--- src/stories/molecules-card.stories.js
+++ src/stories/molecules-card.stories.js
@@ -1,1 +1,7 @@
+import Item_Example from "../components/molecules/card/card.html";
+import "../components/molecules/card/card.scss";
+
+export default { title: "molecules/card" };
+
+export const Example = () => Item_Example;
 
