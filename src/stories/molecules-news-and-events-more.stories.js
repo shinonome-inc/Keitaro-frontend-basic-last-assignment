4550a9d6a0e24be0fae441d20e22cb2f0c096b3a
KeitaroYabu <k.yabu@4nonome.com>
2021-12-08 19:12:46 +0900

    setting .stories.js

--- src/stories/molecules-news-and-events-more.stories.js
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -1,1 +1,6 @@
+import Item_Example from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
 
+export default { title: "molecules/news-and-events-more" };
+
+export const Example = () => Item_Example;
