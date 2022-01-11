6058e80c493e51d30d927fd129e5952b7834672c
KeitaroYabu <k.yabu@4nonome.com>
2022-01-11 20:21:55 +0900

    setting .stories.js

--- src/stories/organisms-news.stories.js
+++ src/stories/organisms-news.stories.js
@@ -1,1 +1,7 @@
+import Item_Example from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "organisms/news" }
+
+export const Example = () => Item_Example;
 
